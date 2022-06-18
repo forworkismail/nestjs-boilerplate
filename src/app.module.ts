373a5610e52ca6feead78f58df5fa881092fcefd
Ismail <forwork.ismail@gmail.com>
2022-06-18 14:13:22 +0430

    1.4 Validating postgres environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
+import Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule, ConfigModule.forRoot()],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
