ce5b00aec4ff67c06bba59880c80c303e8f04b9e
Ismail <forwork.ismail@gmail.com>
2022-06-18 14:03:58 +0430

    1.3 Install @nestjs/config and create .env file with postgres credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
